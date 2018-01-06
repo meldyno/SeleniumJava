fff5ec60a08eb321dc302634c1c3d21e449c5978
meldyno <33669857+meldyno@users.noreply.github.com>
2018-01-06 10:45:51 +0100

    Add files via upload
    skusobny project pre ideu

---
+++ skusobny/src/TestSelenium2.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSelenium2 {
+public static void main(String[] args){
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tomas\\Desktop\\testovanie SW\\selenium\\chromedriver_win32\\chromedriver.exe");
+
+    ChromeDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/");
+        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+        element.sendKeys("abc@gmail.com");
+
+        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+        button.click();
+    }
+}
